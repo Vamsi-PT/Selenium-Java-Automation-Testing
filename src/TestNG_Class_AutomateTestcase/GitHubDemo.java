9a45b5a289d9988e1d194eb3c463daa9fb0ad280
Vamsi-PT <dvamsipt15@gmail.com>
2025-02-20 08:02:42 +0530

    GithubDemoClassPush

--- src/TestNG_Class_AutomateTestcase/GitHubDemo.java
+++ src/TestNG_Class_AutomateTestcase/GitHubDemo.java
@@ -1,14 +1,25 @@
 package TestNG_Class_AutomateTestcase;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class GitHubDemo {
+	
+	WebDriver driver;
+	
   @Test
   public void f() {
+	  
+	  driver.get("https://www.github.com");
   }
   @BeforeTest
   public void beforeTest() {
+	  
+ driver = new ChromeDriver();
+	  
+	  driver.manage().window().maximize();
   }
 
 }
